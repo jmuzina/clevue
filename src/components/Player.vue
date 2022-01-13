8e7f848f4464c4163b18d1c809b0a73d4146ceee
Joe Muzina <jmuzina@pm.me>
2022-01-13 17:14:30 -0500

    init

---
+++ src/components/Player.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="player-page">
+    <PlayerBanner />
+    <div>
+      <Panel title="All Pitches">
+        <PitchPlot />
+      </Panel>
+    </div>
+  </div>
+</template>
+<script>
+import PlayerBanner from "./PlayerBanner.vue";
+import Panel from "./layout/Panel.vue";
+import PitchPlot from "./plots/PitchPlot.vue";
+export default {
+  components: {
+    PlayerBanner,
+    Panel,
+    PitchPlot,
+  },
+  data() {
+    return {
+      playerInfo: {},
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+</style>
---
+++ src/components/PlayerBanner.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="player-banner">
+    <!-- https://img.mlbstatic.com/mlb-photos/image/upload/d_people:generic:headshot:67:current.png/w_426,q_auto:best/v1/people/{imgid}/headshot/67/current -->
+    <img src="" alt="Player Image" />
+    <div class="player-info">
+      <div class="player-name">Name</div>
+      <div class="player-bio">age, pos, throws</div>
+      <div class="player-contract-info">orgName | Svc</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    playerInfo: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/_variables.scss";
+</style>
