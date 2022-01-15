ec7410fb18efe6f9c7d2258c1ed70825bf831678
Joe Muzina <joe.muzina@gmail.com>
2022-01-14 19:43:12 -0500

    better data organization for pitches

--- src/App.vue
+++ src/App.vue
@@ -17,18 +17,16 @@
       </div>
       <div class="pitchData col-md-6">
         <div class="row">
-          <Panel title="All Pitches">
-            <PitchPlot :pitches="selectedPlayerData.pitchData" />
-          </Panel>
-
           <Panel
-            v-for="(item, index) in pitchTypes"
-            v-bind:item="item"
-            v-bind:index="index"
-            v-bind:key="item.pitchType"
-            :title="index"
+            v-for="(pitches, pitchType) in playerPitchData[
+              selectedPlayerData.playerId
+            ]"
+            v-bind:item="pitches"
+            v-bind:index="pitchType"
+            v-bind:key="pitchType"
+            :title="pitchType"
           >
-            <PitchPlot :pitches="Object.values(item)" />
+            <PitchPlot :pitches="Object.values(pitches)" />
           </Panel>
         </div>
       </div>
@@ -46,12 +44,12 @@ export default {
     return {
       playerRoster: this.getAllPlayers(),
       gotPlayerRoster: false,
+      playerPitchData: {},
       selectedPlayerData: {},
-      selectedPitchType: "Fastball",
       sortAscending: true,
       sortKey: "fullName",
 
-      pitchTypes: {},
+      pitchTypes: [],
 
       pitchTypeColors: {
         Fastball: "blue",
@@ -64,27 +62,62 @@ export default {
 
       player_selected: function (selected) {
         this.selectedPlayerData = selected;
-        this.pitchTypes = {};
-        for (const k in this.selectedPlayerData.pitchData) {
-          this.selectedPlayerData.pitchData[k].isVisible = true;
-          this.selectedPlayerData.pitchData[k].radius = 0.04;
-          this.selectedPlayerData.pitchData[k].fill = this.pitchColor(
-            this.selectedPlayerData.pitchData[k]
-          );
-          if (
-            this.pitchTypes[this.selectedPlayerData.pitchData[k].pitchName] ==
-            null
-          )
-            this.pitchTypes[
-              this.selectedPlayerData.pitchData[k].pitchName
-            ] = {};
+        const pid = this.selectedPlayerData.playerId;
+        if (this.playerPitchData[pid] == null) {
+          this.pitchTypes = [];
+          this.playerPitchData[pid] = {
+            "All Pitches": [],
+          };
+          let pitchTypesList = {};
+          for (const k in this.selectedPlayerData.pitchData) {
+            this.selectedPlayerData.pitchData[k].isVisible = true;
+            this.selectedPlayerData.pitchData[k].radius = 0.04;
+            this.selectedPlayerData.pitchData[k].fill = this.pitchColor(
+              this.selectedPlayerData.pitchData[k]
+            );
 
-          this.pitchTypes[this.selectedPlayerData.pitchData[k].pitchName][
-            this.selectedPlayerData.pitchData[k].pitchNum
-          ] = this.selectedPlayerData.pitchData[k];
+            if (
+              pitchTypesList[this.selectedPlayerData.pitchData[k].pitchName] ==
+              null
+            )
+              pitchTypesList[
+                this.selectedPlayerData.pitchData[k].pitchName
+              ] = [];
+
+            pitchTypesList[this.selectedPlayerData.pitchData[k].pitchName].push(
+              this.selectedPlayerData.pitchData[k]
+            );
+            this.playerPitchData[pid]["All Pitches"].push(
+              this.selectedPlayerData.pitchData[k]
+            );
+          }
+
+          let pitchKeys = Object.entries(pitchTypesList);
+          pitchKeys.sort(function (a, b) {
+            if (a[0] < b[0]) return -1;
+            else if (a[0] > b[0]) return 1;
+            else return 0;
+          });
+
+          for (const i in pitchKeys) {
+            //console.log(pitchKeys);
+            const pitchType = pitchKeys[i][0];
+            const pitchList = pitchKeys[i][1];
+
+            if (this.pitchTypes.indexOf(pitchType) === -1)
+              this.pitchTypes.push(pitchKeys[i]);
+            if (this.playerPitchData[pid][pitchType] == null)
+              this.playerPitchData[pid][pitchType] = Object.values(pitchList);
+
+            //console.log("pitch list for this player");
+            //console.log(pitchList);
+          }
+          //console.log("full data");
+          //console.log(this.playerPitchData);
+          console.log("generated data for ", pid, this.playerPitchData[pid]);
+        } else {
+          console.log("already have data for ", pid, this.playerPitchData[pid]);
         }
-        //console.log(this.pitchTypes);
-        //console.log(this.selectedPlayerData.pitchData);
       },
     };
   },
