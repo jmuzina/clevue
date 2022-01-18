7005e77217f03a22edbd39bf04c30fc51a125070
Joe Muzina <joe.muzina@gmail.com>
2022-01-17 20:54:50 -0500

    pitch scrolling tweaks

--- src/components/PitchTable.vue
+++ src/components/PitchTable.vue
@@ -129,7 +129,7 @@ export default {
 
         selectedRow.scrollIntoView({
           behavior: "smooth",
-          block: "nearest",
+          block: "center",
           inline: "nearest",
         });
       });
